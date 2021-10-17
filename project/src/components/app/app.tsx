e32f2584d9361876f129e2e698e3fecf955bd156
WseWild <wsevs@protonmail.com>
2021-10-17 22:27:01 +0300

    module3-task1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
 import MainScreen from '../main/main';
+import ErrorPage from '../error-page/error-page';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+import MoviePage from '../movie-page/movie-page';
+import Review from '../add-review/add-review';
+import Player from '../player/player';
+import {AppRoute, AuthorizationStatus} from '../../const';
+import PrivateRoute from '../private-route/private-route';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
 type AppScreenProps = {
@@ -12,7 +21,36 @@ function App(props: AppScreenProps): JSX.Element {
   const {filmCardTitle} = props;
   const {filmCardYear} = props;
 
-  return <MainScreen filmCardGenre={filmCardGenre} filmCardTitle={filmCardTitle} filmCardYear={filmCardYear} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route  exact path={AppRoute.Root} >
+          <MainScreen filmCardGenre={filmCardGenre} filmCardTitle={filmCardTitle} filmCardYear={filmCardYear} />;
+        </Route>
+        <Route exact path={AppRoute.SignIn} >
+          <SignIn/>
+        </Route>
+        <PrivateRoute>
+          exact
+          path={AppRoute.MyList}
+          render={() => <MyList/>}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        </PrivateRoute>
+        <Route exact path={AppRoute.MoviePage}  >
+          <MoviePage/>
+        </Route>
+        <Route exact path={AppRoute.Review} >
+          <Review/>
+        </Route>
+        <Route exact path={AppRoute.Player}  >
+          <Player/>
+        </Route>
+        <Route>
+          <ErrorPage/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/const.ts
@@ -0,0 +1,15 @@
+export enum AppRoute {
+  Root = '/',
+  SignIn = '/login',
+  MyList = '/mylist',
+  MoviePage = '/films/:id"',
+  Review = '/films/:id/review',
+  Player = '/player/:id',
+}
+
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
